89304741ff20fe56650beaaf9b02f0332e3138e1
Rajkumar Kanase <rajkanase11@gmail.com>
2017-07-22 20:54:22 +0530

    zoom image on hover

--- src/app/master-compo/compo-two.component.ts
+++ src/app/master-compo/compo-two.component.ts
@@ -7,64 +7,64 @@ import { Component, OnInit, Input } from '@angular/core';
 
   <br><br>
    <div class="row">
-   <div class="col-md-3">
+   <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[0]" alt="../assets/a.jpg" width="200px"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[1]" alt="../assets/a.jpg" width="200"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[2]" alt="../assets/a.jpg" width="200px"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[3]" alt="../assets/a.jpg" width="200px"/>
 
     </div>
     </div><br/><br/>
 
     <div class="row">
-   <div class="col-md-3">
+   <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[4]" alt="../assets/a.jpg" width="200px"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[5]" alt="../assets/a.jpg" width="200"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[6]" alt="../assets/a.jpg" width="200px"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[7]" alt="../assets/a.jpg" width="200px"/>
 
     </div>
     </div><br/><br/>
 
     <div class="row">
-   <div class="col-md-3">
+   <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[8]" alt="../assets/a.jpg" width="200px"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[9]" alt="../assets/a.jpg" width="200"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[10]" alt="../assets/a.jpg" width="200px"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[11]" alt="../assets/a.jpg" width="200px"/>
 
     </div>
     </div><br/><br/>
 
     <div class="row">
-   <div class="col-md-3">
+   <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[12]" alt="../assets/a.jpg" width="200px"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[13]" alt="../assets/a.jpg" width="200"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[14]" alt="../assets/a.jpg" width="200px"/>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 img-hover">
     <img [src]="'../assets/'+imgNm.imgs[15]" alt="../assets/a.jpg" width="200px"/>
 
     </div>
@@ -76,6 +76,24 @@ import { Component, OnInit, Input } from '@angular/core';
     img{
       border : 2px solid black;
     }
+
+.img-hover img {
+    -webkit-transition: all .3s ease; /* Safari and Chrome */
+  	-moz-transition: all .3s ease; /* Firefox */
+  	-o-transition: all .3s ease; /* IE 9 */
+  	-ms-transition: all .3s ease; /* Opera */
+  	transition: all .3s ease;
+}
+.img-hover img:hover {
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    -webkit-transform:translateZ(0) scale(1.20); /* Safari and Chrome */
+    -moz-transform:scale(1.20); /* Firefox */
+    -ms-transform:scale(1.20); /* IE 9 */
+    -o-transform:translatZ(0) scale(1.20); /* Opera */
+    transform:translatZ(0) scale(1.20);
+}
+
     `
   ]
 })
